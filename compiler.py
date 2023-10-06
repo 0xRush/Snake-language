e8863c447960978da2214b53ebf877047070a328
0xRush <eias742@gmail.com>
2023-10-06 19:04:03 +0300

    comp project

---
+++ compiler.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, flash
+import basic
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        req = request.form["workspace"].strip()
+        result, error = basic.run('<stdin>', req)
+        if error:
+            flash(f'حدث خطأ ما', 'danger')
+            return render_template('home.html', res=error.as_string(), req=req)
+        elif result:
+            if len(result.elements) == 1:
+                flash(f'نجح الأمر', 'success')
+                return render_template('home.html', res=repr(result.elements[0]), req=req)
+            else:
+                flash(f'نجح الأمر', 'success')
+                return render_template('home.html', res=repr(result), req=req)
+    
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
