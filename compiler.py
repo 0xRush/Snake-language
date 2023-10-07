ed68e250f3a046b022cae68aa8e74a90c19c98fb
0xRush <eias742@gmail.com>
2023-10-07 20:34:03 +0300

    added docs page

--- compiler.py
+++ compiler.py
@@ -22,6 +22,9 @@ def home():
     
     return render_template('home.html')
 
+@app.route("/docs")
+def docs():
+    return render_template('docs.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
